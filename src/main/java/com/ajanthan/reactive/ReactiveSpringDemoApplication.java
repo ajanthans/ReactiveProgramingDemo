b14b0efb4a66bca27cad9f7ed695b64b52e58afd
ajanthans <vvsaja@gmail.com>
2020-04-10 17:49:36 +0800

    Local chnages

---
+++ src/main/java/com/ajanthan/reactive/ReactiveSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ajanthan.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringDemoApplication.class, args);
+    }
+
+}
